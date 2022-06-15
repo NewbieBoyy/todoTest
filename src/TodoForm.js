4b4726e7ebaec19be7e3153b7556164888691008
unknown <eliemerhej003@gmail.com>
2022-06-15 12:59:28 +0300

    first commit

---
+++ src/TodoForm.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react';
+    function TodoForm () {
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState([])
+    const [state, setState] = useState([])
+    
+    const handleChange = e => {
+        setInput(e.target.value);
+    }
+    const handleSubmit =e => {
+        e.preventDefault();
+        
+        const newTodos = [input,...todos]
+        setTodos(newTodos)
+        console.log(...todos)
+    }
+    const removeTodo = index => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+      };
+    const  editTodo = (index) => {
+        const newTodo = prompt('Let\'s make something changes')
+        const todos = state;
+        todos.filter(todo => {
+          if (todo.id === index + 1) {
+            todo.title = newTodo;
+          }
+          return todo;
+        });
+        setState({ todos });
+        
+      }
+    return  (
+       <div>
+           <div>
+          <form className= 'todo-form'>
+            <input
+            type ='text'
+            placeholder='Add a todo'
+            value={input}
+            name = 'text'
+            className='todo-input'
+            onChange ={handleChange}
+            />
+              
+            <button className='todo-button' onClick={handleSubmit}>Add todo</button>  
+           
+            
+            
+           </form>
+        {todos.map((todo, index) => (<div key={index}><p>{todo}</p><button onClick={() => removeTodo(index)}> remove </button></div>))}
+        {todos.map((todo, index) => (<div key={index}><p>{todo}</p><button onClick={() => editTodo(index)}> update </button></div>))}
+        </div>
+
+       </div>
+
+      
+    )
+}
+
+
+export default TodoForm;
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TodoForm from './TodoForm'
+import reportWebVitals from './reportWebVitals';
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TodoForm />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
