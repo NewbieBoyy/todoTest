bb11aa45c1cfeb232caa6cb5a6b39dc9ab669088
Walid Hawari <walidhawari95@gmail.com>
2022-06-15 14:12:42 +0300

    [UPDATE]: todos

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,64 +1,81 @@
-import React, {useState} from 'react';
-    function TodoForm () {
-    const [input, setInput] = useState('');
-    const [todos, setTodos] = useState([])
-    const [state, setState] = useState([])
-    
-    const handleChange = e => {
-        setInput(e.target.value);
-    }
-    
-    const handleSubmit =e => {
-        e.preventDefault();
-        
-        const newTodos = [input,...todos]
-        setTodos(newTodos)
-        console.log(...todos)
-    }
-    const removeTodo = index => {
-        const newTodos = [...todos];
-        newTodos.splice(index, 1);
-        setTodos(newTodos);
-      };
-    const  editTodo = (index) => {
-        const newTodo = prompt('Let\'s make something changes')
-        const todos = state;
-        todos.filter(todo => {
-          if (todo.id === index + 1) {
-            todo.title = newTodo;
-          }
-          return todo;
-        });
-        setState({ todos });
-        
-      }
-    return  (
-       <div>
-           <div>
-          <form className= 'todo-form'>
-            <input
-            type ='text'
-            placeholder='Add a todo'
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const TodoForm = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: uuidv4(),
+      title: input,
+    };
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    setInput("");
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.splice(id, 1);
+    setTodos(newTodos);
+  };
+
+  const editTodo = (oldTodo) => {
+    // oldTodo = {
+    //   id: "",
+    //   title: "",
+    // };
+    // const updatedTodos = [{ id: "1", title: "Elie" }];
+    // todos.filter((todo) => {
+    //   console.log("Todo Id", todo.id);
+    //   // return todo;
+    // });
+    // setState({ todos });
+  };
+
+  // bas tekbes 3al edit button
+  // add the todo title to the input box
+  // Change the add todo button to confirm button
+
+  return (
+    <div>
+      <div>
+        <form className="todo-form">
+          <input
+            type="text"
+            placeholder="Add a todo"
+            name="text"
+            className="todo-input"
             value={input}
-            name = 'text'
-            className='todo-input'
-            onChange ={handleChange}
-            />
-              
-            <button className='todo-button' onClick={handleSubmit}>Add todo</button>  
-           
-            
-            
-           </form>
-        {todos.map((todo, index) => (<div key={index}><p>{todo}</p><button onClick={() => removeTodo(index)}> remove </button></div>))}
-        {todos.map((todo, index) => (<div key={index}><p>{todo}</p><button onClick={() => editTodo(index)}> update </button></div>))}
-        </div>
+            onChange={handleChange}
+          />
 
-       </div>
+          <button className="todo-button" onClick={handleSubmit}>
+            Add todo
+          </button>
+        </form>
 
-      
-    )
-}
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <span>{todo.title}</span>
 
+            <button onClick={() => removeTodo(todo.id)}> remove </button>
+
+            <button onClick={() => editTodo(todo)}> Edit </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default TodoForm;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import TodoForm from './TodoForm'
-import reportWebVitals from './reportWebVitals';
-import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import TodoForm from "./TodoForm";
+import reportWebVitals from "./reportWebVitals";
+import { toHaveStyle } from "@testing-library/jest-dom/dist/matchers";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <TodoForm />
